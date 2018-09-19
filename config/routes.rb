f405545a6d7939c6fb5ac2b312053d56fea8519a
root_suke <rootsuke@gmail.com>
2018-09-20 00:43:35 +0900

    利用規約ページを追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/help", to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
+  get "/agreement", to: "static_pages#agreement"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users do

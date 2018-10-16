758d0d7550dfda76f3c2cc025810f1511901b3e6
root_suke <rootsuke@gmail.com>
2018-10-17 00:17:23 +0900

    本番環境でのmailerのバグを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'lit-brushlands-90692.herokuapp.com'
+  host = 'clone-insta-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

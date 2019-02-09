0f6302f531b8f8e251b7265e0f2fadd449108df3
root_suke <rootsuke@gmail.com>
2019-02-10 00:33:44 +0900

    fix routing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,36 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
   root "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/help", to: "static_pages#help"
   get "/contact", to: "static_pages#contact"
   get "/agreement", to: "static_pages#agreement"
-  get "/signup", to: "users#new"
-  post "/signup", to: "users#create"
+
+  get    "/signup", to: "users#new"
+  post   "/signup", to: "users#create"
+  get 'sessions/new'
+  get    "/login",  to: "sessions#new"
+  post   "login",   to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  get 'password_resets/new'
+  get 'password_resets/edit'
+
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :comments,            only: :create
+  resources :relationships,       only: [:create, :destroy]
+  resources :favorites,           only: [:create, :destroy]
+  resources :notifications,       only: :index
+
   resources :users do
     member do
       get :following, :followers, :favorites, :change_password, :more_favorites, :more_microposts
     end
   end
 
-  get "/login", to: "sessions#new"
-  post "login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
-
   resources :microposts do
     member do
       post :favorite
     end
   end
-
-  resources :comments, only: :create
-  resources :relationships, only: [:create, :destroy]
-  resources :favorites, only: [:create, :destroy]
-  resources :notifications, only: :index
-
 end
